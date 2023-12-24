13dbd9f8bbcbd9e714765e2a65724dc2139ad8de
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2023-12-24 17:42:07 +0100

    Update assistent.py

--- assistent.py
+++ assistent.py
@@ -2,6 +2,10 @@
 Liste mit Custom GPTs
 """
 
+import os
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
 gpts = {
     "kirenz": os.getenv('OPENAI_ASSISTANT'),
     "ContentCrafter": os.getenv('ContentCrafter'),
