68e553713391eb1432c1e3c5110bd9ec67f9e0cb
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2023-12-24 16:58:48 +0100

    Create assistent.py

---
+++ assistent.py
@@ -0,0 +1,9 @@
+"""
+Liste mit Custom GPTs
+"""
+
+gpts = {
+    "kirenz": os.getenv('OPENAI_ASSISTANT'),
+    "ContentCrafter": os.getenv('ContentCrafter'),
+    "PersonaTutor": os.getenv('PersonaTutor')
+}
